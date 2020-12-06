c629d272a93a7411c42e43fd615fde2f8abfecc4
James Edgeley <j.edgeley.edge@gmail.com>
2020-12-06 10:43:50 +0000

    commit

---
+++ day6.py
@@ -0,0 +1,25 @@
+_list=list(open("day6.txt").read().split('\n\n'))
+
+def firstpart():
+    count=0
+    for group in _list:
+        questions=[]
+        for char in group:
+            questions.append(char)
+        uniques=set(questions)
+        uniques.discard('\n')
+        count+=len(uniques)
+    return count
+
+print(firstpart())
+
+def secondpart():
+    count=0
+    for group in _list:
+        individuals=group.splitlines()
+        for char in individuals[0]:
+            if group.count(char)==len(individuals):
+                count+=1
+    return count
+
+print(secondpart())
