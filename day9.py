d3fb558e01943c50cc4343ac716749d0e7680bc8
James Edgeley <j.edgeley.edge@gmail.com>
2020-12-09 09:30:36 +0000

    commit

---
+++ day9.py
@@ -0,0 +1,22 @@
+_list=list(open("day9.txt").read().splitlines())
+data=[int(i) for i in _list]
+
+def firstpart():
+    for i in range(25,len(data)):
+        combinations=[]
+        for j in range(1,26):
+            for k in range(1,j):
+                combinations.append(data[i-j]+data[i-k])
+        if data[i] not in combinations:
+            return data[i]
+
+print(firstpart())
+
+def secondpart():
+    target=firstpart()
+    for i in range(0,len(data)):
+        for j in range(i+1,len(data)):
+            if sum(data[i:j+1])==target:
+                return max(data[i:j+1])+min(data[i:j+1])
+
+print(secondpart())
